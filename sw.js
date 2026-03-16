ef1a6d8e3c73e369f33dda24dffe8dd537b6a3c8
lettuceguess-cell <lettuceguess@gmail.com>
2026-03-16 21:40:17 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,57 @@
-const CACHE_NAME = "lettuce-guess-v2";
-const assets = ["./", "./index.html", "./styles.css", "./script.js", "./manifest.json", "./logo.png"];
+const CACHE_NAME = "lettuce-guess-v3";
 
+const assets = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./script.js",
+  "./manifest.json",
+  "./logo.png"
+];
+
+// Install the new update
 self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(assets)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+  self.skipWaiting();
 });
 
+// Clean up old memory
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// NETWORK FIRST STRATEGY (Solves the caching issue completely)
+// It always checks the live internet first to get your latest updates. 
+// It only loads from memory if the user's internet is turned off.
 self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(
+    fetch(e.request)
+      .then((response) => {
+        // If internet works, save the newest version to memory
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(e.request, clone);
+        });
+        return response;
+      })
+      .catch(() => {
+        // If internet is off, load the saved app from memory
+        return caches.match(e.request);
+      })
+  );
 });
